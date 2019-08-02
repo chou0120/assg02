5acbe2b22243656104a62bdc2be230945a24ef56
chou0120 <chou0120@users.noreply.github.com>
2019-08-02 19:02:26 -0400

    deployed to firebase

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="home">
-   <h2>PLease login in or sign up using the approcaite tabs to see the app</h2>
+   <h2>PLease login in or sign up using the apporiate tabs to see the app</h2>
   </div>
 </template>
 
